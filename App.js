08a9b0c55f4b73825966b75704b017e73f771bd3
davilocksp <davilocks6@gmail.com>
2025-07-06 23:16:34 -0300

    finalizado o exercicio

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import Login from "./Screens/Login";
 import Home from "./Screens/Home";
-
+import Feed from "./Screens/Feed";
 
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 function BottomTabs(){
   const BottomTab = createBottomTabNavigator();
-  return(
-    <NavigationContainer>
-      <BottomTab.Navigator>
-        <BottomTab.Screen
-          name="HomeTab"
-          component={Home}
-        />
-      </BottomTab.Navigator>
-    </NavigationContainer>
 
+  return(
+    <BottomTab.Navigator>
+      <BottomTab.Screen name = "HomeTab" component={Home}
+      options={{tabBarIcon: () => <AntDesign name="home" size={24} color="black" />}}
+      />
+      <BottomTab.Screen name = "FeedTab" component={Feed}
+      options={{tabBarIcon: () => <FontAwesome name="feed" size={24} color="black" />}}
+      />
+    </BottomTab.Navigator>
   )
 }
 
@@ -28,6 +30,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name = "TelaLogin" component={Login}
+        options={{headerShown: false}}
         />
         <Stack.Screen options = {{headerShown: false}} name ="HomeTab" component= {BottomTabs} />
       </Stack.Navigator>
--- Screens/Feed.js
+++ Screens/Feed.js
@@ -1,173 +1,62 @@
-import { View, StyleSheet, Text, Image, ScrollView, TouchableOpacity } from "react-native";
-import { use, useState } from "react";
+import { View, Image, Text, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
 
-export default function Home(){
+export default function Feed(){
 
-    const [contador1, setContador1] = useState(0);
-    const [contador2, setContador2] = useState(0);
-    const [contador3, setContador3] = useState(0);
-    const [contador4, setContador4] = useState(0);
-    const [contador5, setContador5] = useState(0);
-
-    function Aumentar1(){
-        setContador1(contador1 + 1);
-    }
-    function Diminuir1(){
-        if (contador1 > 0 )    
-            setContador1(contador1 - 1);
-    }
-    function Aumentar2(){
-        setContador2(contador2 + 1);
-    }
-    function Diminuir2(){
-        if (contador2 > 0)
-            setContador2(contador2 - 1);
-    }
-    function Aumentar3(){
-        setContador3(contador3 + 1);
-    }
-    function Diminuir3(){
-        if (contador3 > 0)
-            setContador3(contador3 - 1);
-    }
-    function Aumentar4(){
-        setContador4(contador4 + 1);
-    }
-    function Diminuir4(){
-        if (contador4 > 0)
-            setContador4(contador4 - 1);
-    }
-    function Aumentar5(){
-        setContador5(contador5 + 1);
-    }
-    function Diminuir5(){
-        if (contador5 > 0)
-            setContador5(contador5 - 1);
-    }
-
+    const [menssagem, setMenssagem] = useState("")
 
     return(
-        <ScrollView>
-            <View style={styles.contairnerProfile}>
-                <Text>
-                    Estoque
-                </Text>
+        <View style={styles.container}>
+            <Image 
+            style = {styles.img}
+            source={uri = 'https://img.cdndsgni.com/preview/10057856.jpg'}/>
 
-                <View style={styles.viewImg}>
-                    <Image
-                        style={styles.img}
-                        source={{ uri: 'https://media.istockphoto.com/id/182897295/de/foto/pinkes-canvas-schuh.jpg?s=612x612&w=0&k=20&c=7t90JNzKUr3DYUTAlG_hgUKlEjtmu-aJQbHjmeRkd5M=',}}
-                    />
-                    <Text>
-                        Descrição breve
-                    </Text>
+            <Text style={styles.txtTitulo}>Fale Conosco</Text>
 
-                    <Text>{contador1}</Text>
+            <TextInput
+            style = {styles.viewInput}
+            placeholder="Nome"
+            />
+            <TextInput
+            style = {styles.viewInput}
+            placeholder="E-Mail"
+            />
+            <TextInput
+            style = {styles.viewInput}
+            placeholder="Menssagem"
+            value={menssagem}
+            onChangeText={setMenssagem}
+            />
 
-                    <TouchableOpacity
-                    onPress={Aumentar1}>
-                        <Text>+</Text>
-                    </TouchableOpacity>
+            <Text>Menssagem: {menssagem}</Text>
+        </View>
 
-                    <TouchableOpacity
-                    onPress={Diminuir1}>
-                        <Text>-</Text>
-                    </TouchableOpacity>
-
-                    <Image
-                    style={styles.img}
-                    source={{uri: 'https://img.freepik.com/fotos-gratis/tenis-de-corrida-esportivos_1203-3414.jpg?semt=ais_hybrid&w=740'}}
-                    />
-                    <Text>
-                        Descrição breve
-                    </Text>
-
-                    <Text>{contador2}</Text>
-
-                    <TouchableOpacity
-                    onPress={Aumentar2}
-                    >
-                        <Text>+</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                    onPress={Diminuir2}
-                    >
-                        <Text>-</Text>
-                    </TouchableOpacity>
-
-                    <Image
-                    style={styles.img}
-                    source={{uri: 'https://media.istockphoto.com/id/491603822/pt/foto/par-de-sapatos-desportivos-verde.jpg?s=612x612&w=0&k=20&c=XFNzktqFxao2qt4P1NFBBzmq8LelubVXdXQKXth1AbE='}}
-                    />
-
-                    <Text>
-                        Descrição breve
-                    </Text>
-
-                    
-                    <TouchableOpacity
-                    onPress={Aumentar3}
-                    >
-                        <Text>+</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                    onPress={Diminuir3}
-                    >
-                        <Text>-</Text>
-                    </TouchableOpacity>
-
-                    <Image
-                    style={styles.img}
-                    source={{uri: 'https://media.istockphoto.com/id/1688015574/pt/foto/white-sneaker-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=2nzu1e2LMqe2GR0wga2iytvAM7poL2ppDiX_2HZZMGU='}}
-                    />
-                    <Text>
-                        Descrição breve
-                    </Text>
-                    <TouchableOpacity
-                    onPress={Aumentar4}
-                    >
-                        <Text>+</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    onPress={Diminuir4}
-                    >
-                        <Text>-</Text>
-                    </TouchableOpacity>
-
-                    <Image
-                    style={styles.img}
-                    source={{uri: 'https://vizzent.vtexassets.com/arquivos/ids/626980-400-400/tenis-casual-masculino-mormaii-preto-203364-3-.jpg?v=638768688541970000'}}
-                    />
-                    <Text>
-                        Descrição breve
-                    </Text>
-
-                    <TouchableOpacity
-                    onPress={Aumentar5}
-                    >
-                        <Text>+</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    onPress={Diminuir5}
-                    >
-                        <Text>-</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    contairnerProfile:{
-        alignItems: 'center',
-        padding: 20,
-        // borderWidth: 1,
-    },
-    img:{
-        height: 150,
-        width: 150
-    }
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+    alignItems: 'center'
+  },
+  img: {
+    height: 200,
+    width: 350
+  },
+  viewInput: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    borderRadius: 12,
+    margin: 12,
+    borderWidth: 1,
+    height: 30,
+    width: 150
+  },
+  txtTitulo: {
+    fontSize: 26,
+    marginTop: 40
+  }
+
 })
